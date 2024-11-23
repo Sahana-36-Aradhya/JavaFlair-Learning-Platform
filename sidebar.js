3f13a7990c626f672e5f7a7c1628e9085c6d0075
Sahana Aradhya <sahanaaradhya02@gmail.com>
2024-11-23 11:01:17 +0530

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,27 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // Load the sidebar content
-  fetch('HTML/sidebar.html') // Adjust the path if necessary
+  // Check screen width and load sidebar only for larger screens
+  if (window.innerWidth > 480) { 
+    // Load the sidebar content for desktop/tablet
+    fetch('HTML/sidebar.html') // Adjust the path if necessary
       .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok: ' + response.statusText);
-          }
-          return response.text();
+        if (!response.ok) {
+          throw new Error('Network response was not ok: ' + response.statusText);
+        }
+        return response.text();
       })
       .then(data => {
-          document.getElementById('sidebar-nav').innerHTML = data;
+        document.getElementById('sidebar-nav').innerHTML = data;
 
-          // Set the active class for the current page
-          const currentLocation = window.location.pathname.split('/').pop();
-          const navLinks = document.querySelectorAll('#sidebar-nav ul li a');
+        // Set the active class for the current page
+        const currentLocation = window.location.pathname.split('/').pop();
+        const navLinks = document.querySelectorAll('#sidebar-nav ul li a');
 
-          navLinks.forEach(link => {
-              if (link.getAttribute('href') === currentLocation) {
-                  link.classList.add('active');
-              }
-          });
+        navLinks.forEach(link => {
+          if (link.getAttribute('href') === currentLocation) {
+            link.classList.add('active');
+          }
+        });
 
-          // Scroll to the top of the page after loading the sidebar
-          window.scrollTo(0, 0);
+        // Scroll to the top of the page after loading the sidebar
+        window.scrollTo(0, 0);
       })
       .catch(error => console.error('Error loading sidebar:', error));
+  }
 });
